b49ef3961ed37e38d3814c246c732a262d295af1
VarunReddyT <tvarun2014@gmail.com>
2025-02-19 14:14:04 +0530

    Day 8

--- 08 18-02-25/UpsideDown.java
+++ 08 18-02-25/UpsideDown.java
@@ -1,53 +1,3 @@
-// package 08 18-02-25;
-
-// public class u {
-    
-// }
-// VishnuVardan is working with Decision Trees for AI-based predictions.
-// To analyze alternative outcomes, Kishore has planned to flip the decision 
-// tree horizontally to simulate a reverse processing approach.
-
-// Rules for Flipping the Decision Tree:
-// 	- The original root node becomes the new rightmost node.
-// 	- The original left child becomes the new root node.
-// 	- The original right child becomes the new left child.
-// This transformation is applied level by level recursively.
-
-// Note:
-// ------
-// - Each node in the given tree has either 0 or 2 children.
-// - Every right node in the tree has a left sibling sharing the same parent.
-// - Every right node has no further children (i.e., they are leaf nodes).
-
-// Your task is to help VishnuVardan flip the Decision Tree while following 
-// the given transformation rules.
-
-// Input Format:
-// -------------
-// Space separated integers, nodes of the tree.
-
-// Output Format:
-// --------------
-// Print the list of nodes of flipped tree as described below.
-
-
-// Sample Input-1:
-// ---------------
-// 4 2 3 5 1
-
-// Sample Output-1:
-// ----------------
-// 5 1 2 3 4
-
-
-// Sample Input-2:
-// ---------------
-// 4 2 5
-
-// Sample Output-2:
-// ----------------
-// 2 5 4
-
 import java.util.*;
 
 class BinaryTree{
@@ -96,10 +46,24 @@ public class UpsideDown{
         root.right = null;
         return node;
     }
+    public static void levelOrderTraversal(BinaryTree root) {
+        if (root == null) return;
+        
+        Queue<BinaryTree> queue = new LinkedList<>();
+        queue.offer(root);
+        
+        while (!queue.isEmpty()) {
+            BinaryTree node = queue.poll();
+            System.out.print(node.data + " ");
+            
+            if (node.left != null) queue.offer(node.left);
+            if (node.right != null) queue.offer(node.right);
+        }
+    }
     public static void solve(int[] levelorder){
         BinaryTree root = buildTree(levelorder);
         BinaryTree newRoot = flip(root);
-        System.out.println(newRoot.data);
+        levelOrderTraversal(newRoot);
         
     }
     public static void main(String[] args){
