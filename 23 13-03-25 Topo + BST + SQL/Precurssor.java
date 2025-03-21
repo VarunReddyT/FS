2562d78bacc8e2bf3d08cdca99eb8ca4e5759d45
VarunReddyT <tvarun2014@gmail.com>
2025-03-21 10:34:53 +0530

    Queue

--- 23 13-03-25 Topo + BST + SQL/Precurssor.java
+++ 23 13-03-25 Topo + BST + SQL/Precurssor.java
@@ -97,7 +97,7 @@ public class Precurssor{
     }
     public static void main(String[] args){
         Scanner sc = new Scanner(System.in);
-        int n = sc.nextInt();
+        // int n = sc.nextInt();
         int m = sc.nextInt();
         int k = sc.nextInt();
         int[][] arr = new int[m][2];
