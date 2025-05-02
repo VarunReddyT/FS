1475ade23c26c609d494699fceebb8fee193adfb
VarunReddyT <tvarun2014@gmail.com>
2025-05-02 22:15:56 +0530

    Day44

---
+++ 44 02-05-25/WaterFill.java
@@ -0,0 +1,45 @@
+/*
+A Kid built a structure using building blocks,
+by placing the building-blocks adjacent to each other.
+
+A building-block is a vertical alignment of blocks.
+	                            ___
+here one block each represents  as |_|.
+
+The following structure made up of using building blocks
+
+                          ___
+                      __||    ___
+                     |||w||__              ___
+                  __|||||| w   w  w |_|
+              __|||||||_w||w||___________
+
+               0  1   3   4   2   3    2   0   1   0   2
+
+Once the structure is completed, kid pour water(w) on it.
+
+You are given a list of integers, heights of each building-block in a row.
+ Now your task How much amount of water can be stored by the structure.
+
+ Input Format:
+ -------------
+ Space separated integers, heights of the blocks in the structure.
+
+Output Format:
+ --------------
+ Print an integer,
+
+Sample Input:
+-------------
+ 0 1 3 4 2 3 2 0 1 0 2
+
+Sample Output:
+--------------
+6
+
+Explanation:
+ -----------
+In the above structure,  6 units of water (w represents the water in the structure)
+can be stored.
+
+ */
