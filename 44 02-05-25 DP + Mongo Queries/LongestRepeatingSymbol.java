e55c31ea07ec68bf97ad6ab423c1ed879277d1a7
VarunReddyT <tvarun2014@gmail.com>
2025-05-03 18:18:43 +0530

    Day44

--- 44 02-05-25 DP + Mongo Queries/LongestRepeatingSymbol.java
+++ 44 02-05-25 DP + Mongo Queries/LongestRepeatingSymbol.java
@@ -21,3 +21,31 @@ Explanation: The longest repeating symbol pattern is "scarab", appearing twice.
 - 'text' consists of lowercase English letters ('a' - 'z').
 
  */
+
+import java.util.*;
+
+public class LongestRepeatingSymbol{
+    public static int getRepeatingSubtring(String s, int n){
+        int[][] dp = new int[n+1][n+1];
+        int maxLen = 0;
+        for(int i = 1;i<=n;i++){
+            for(int j = 1;j<=n;j++){
+                if(s.charAt(i-1) == s.charAt(j-1) && i!=j){
+                    dp[i][j] = dp[i-1][j-1] + 1;
+                }
+                else{
+                    dp[i][j] = 0;
+                }
+                maxLen = Math.max(maxLen,dp[i][j]);
+            }
+        }
+        return maxLen;
+    }
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        String text = sc.nextLine();
+        int n = text.length();
+        System.out.println(getRepeatingSubtring(text, n));
+        sc.close();
+    }
+}
--- 44 02-05-25 DP + Mongo Queries/MinHealthPoints.java
+++ 44 02-05-25 DP + Mongo Queries/MinHealthPoints.java
@@ -51,3 +51,37 @@ Explanation:
 start at cell(0,0) =>  8→6→6→2→1→9→2 minimizes the loss of health points.
 
  */
+
+import java.util.*;
+
+public class MinHealthPoints{
+    public static int getMinHealthPoints(int[][] grid, int m, int n){
+        int[][] dp = new int[m][n];
+        dp[0][0] = grid[0][0];
+        for(int i = 1;i<m;i++){
+            dp[i][0] = grid[i][0] + dp[i-1][0];
+        }
+        for(int i = 1;i<n;i++){
+            dp[0][i] = grid[0][i] + dp[0][i-1];
+        }
+        for(int i = 1;i<m;i++){
+            for(int j = 1;j<n;j++){
+                dp[i][j] = grid[i][j] + Math.min(dp[i-1][j],dp[i][j-1]);
+            }
+        }
+        return dp[m-1][n-1];
+    }
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int m = sc.nextInt();
+        int n = sc.nextInt();
+        int[][] grid = new int[m][n];
+        for(int i=0;i<m;i++){
+            for(int j=0;j<n;j++){
+                grid[i][j] = sc.nextInt();
+            }
+        }
+        System.out.println(getMinHealthPoints(grid, m, n));
+        sc.close();
+    }
+}
