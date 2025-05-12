07f32280a1d439492a308146bc1d4f5203294023
VarunReddyT <tvarun2014@gmail.com>
2025-05-12 17:16:05 +0530

    Day49

--- 49 12-05-25 BFS + Greedy + SQL + React/react/src/App.jsx
+++ 49 12-05-25 BFS + Greedy + SQL + React/react/src/App.jsx
@@ -1,8 +1,19 @@
 import React from 'react';
 import Data from './components/Data';
+import Greeting from './components/Greeting';
+import Counter from './components/Counter';
+import Navbar from './components/Navbar';
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 function App() {
   return (
-      <Data/>
+    <Router>
+        <Navbar />
+        <Routes>
+          <Route path="/" element={<Greeting name="KMIT" />} />
+          <Route path="/counter" element={<Counter />} />
+          <Route path="/data" element={<Data />} />
+        </Routes>
+    </Router>
   )
 }
 
