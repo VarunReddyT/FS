a07a857cab59cdf2675dc48d94aaa13c3196fdab
VarunReddyT <tvarun2014@gmail.com>
2025-05-30 18:01:52 +0530

    Day62

--- 62 30-05-25 DP + React/ps_5_6/backend/index.js
+++ 62 30-05-25 DP + React/ps_5_6/backend/index.js
@@ -3,7 +3,9 @@ const express = require('express');
 const mongoose = require('mongoose');
 const cors = require('cors');
 const Product = require('./models/Product');
-
+const User = require('./models/User');
+const bcrypt = require('bcryptjs');
+const jwt = require('jsonwebtoken');
 const app = express();
 app.use(cors());
 app.use(express.json());
@@ -49,6 +51,38 @@ app.delete('/api/products/:id', async (req, res) => {
     res.status(400).json({ error: 'Delete failed' });
   }
 });
+
+app.post("/api/register", async (req, res) => {
+  const {username,password,role} = req.body;
+  try {
+    const hashedPassword = await bcrypt.hash(password, 10);
+    const newUser = new User({ username, password: hashedPassword,role });
+    await newUser.save();
+    res.status(201).json({ success: true, message: 'Registration successful. You can now login' });
+  } catch (err) {
+    console.error(err); 
+    res.status(500).json({ error: 'Server error', message: `Registration failed: ${err.message}` });
+  }
+});
+
+app.post("/api/login", async (req, res) => {
+  const { username, password } = req.body;
+  try {
+    const user = await User.findOne({ username });
+    if (!user) {
+      return res.status(400).json({ error: 'Invalid username or password' });
+    }
+    const isMatch = await bcrypt.compare(password, user.password);
+    if (!isMatch) {
+      return res.status(400).json({ error: 'Invalid username or password' });
+    }
+    const token = jwt.sign({ id: user._id }, '9618443777', { expiresIn: '6h' });
+    res.json({ success: true, token,role : user.role, user: { id: user._id, username: user.username } });
+  } catch (err) {
+    console.error(err);
+    res.status(500).json({ error: 'Server error', message: `Login failed. Please try again.` });
+  }
+  });
 
 const PORT = 5000;
 app.listen(PORT, () => {
