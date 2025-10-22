331b4c4e9d9584a78c693dc049f4f5e16365ddab
VarunReddyT <tvarun2014@gmail.com>
2025-10-22 14:31:58 +0530

    Day109

--- 109 22-10-25 Topo Sort + Trees/LCA_Main.py
+++ 109 22-10-25 Topo Sort + Trees/LCA_Main.py
@@ -30,6 +30,39 @@ Dr.Alpha
 
 '''
 
+'''
+In a hierarchical research institute, every scientist reports to a supervising 
+scientist. Given two scientists, find the nearest common supervisor (LCS) who 
+oversees both (directly or indirectly).
+
+                Director
+                /      \
+         Dr.Alpha       Dr.Delta
+         /     \          /     \
+   Dr.Beta   Dr.Gamma  Dr.Epsilon  Dr.Zeta
+
+Input Format
+------------
+You’ll be given:
+    - A set of parent–child relationships defining the hierarchy.
+    - Two scientist names whose nearest common supervisor needs to be found.
+
+Output Format:
+--------------
+A string, Name of LCS
+
+Sample Input:
+-------------
+{"Director": None, "Dr.Alpha": "Director", "Dr.Delta": "Director", "Dr.Beta": "Dr.Alpha", "Dr.Gamma": "Dr.Alpha", "Dr.Epsilon": "Dr.Delta", "Dr.Zeta": "Dr.Delta"}
+("Dr.Beta", "Dr.Gamma")
+
+Sample Output:
+--------------
+Dr.Alpha
+
+'''
+import json
+
 class Node:
     def __init__(self,value):
         self.value = value
@@ -47,8 +80,6 @@ def build_tree(hierarchy):
         if parent is None:
             root = child_node
         else:
-            if parent not in nodes:
-                nodes[parent] = Node(parent)
             parent_node = nodes[parent]
             
             if parent_node.left is None:
@@ -63,9 +94,9 @@ def lca(root, a, b):
     right = lca(root.right,a,b)
     if left and right:
         return root
-    if left:
-        return left
-    return right
+    if left is None:
+        return right
+    return left
 
 hierarchy = eval(input())
 
@@ -74,4 +105,3 @@ name1, name2 = eval(input())
 root = build_tree(hierarchy)
 
 print(lca(root,name1,name2).value)
-# print(hierarchy)
